6ee9c0d76abf683dc3aae98840878b567f269714
DarynaTorianyk <toryanik680@gmail.com>
2023-11-30 13:56:31 +0200

    [CPA-12]:Create calculate logic (addition, subtraction, multiplication, division)

--- script.js
+++ script.js
@@ -23,22 +23,139 @@ function toggleTheme() {
 
 
 const buttons = document.querySelectorAll('.buttons__buttonNumbers');
+const calculatorInput = document.querySelector('.calculatorInput');
+const subCalculatorInput = document.querySelector('.subCalculatorInput');
+
+let currentInput = '';
+let previousInput = '';
+let operator = '';
+
+function updateDisplay() {
+  if (previousInput !== '') {
+    calculatorInput.value = currentInput;
+    subCalculatorInput.value = `${previousInput}${operator}${currentInput}`;
+  } else {
+    calculatorInput.value = currentInput;
+    subCalculatorInput.value = currentInput;
+  }
+}
+
+function handleNumberClick(number) {
+  currentInput += number;
+  updateDisplay();
+}
+
+function handleOperatorClick(op) {
+  if (currentInput !== '') {
+    if (previousInput !== '') {
+      performCalculation();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+    updateDisplay();
+  }
+}
+
+function handleEqualsClick() {
+  if (previousInput !== '' && currentInput !== '') {
+    performCalculation();
+    operator = '';
+  }
+}
+
+function handleClearClick() {
+  currentInput = '';
+  previousInput = '';
+  operator = '';
+  updateDisplay();
+}
+
+function handlePlusMinusClick(currentInput) {
+  if (currentInput !== '') {
+    currentInput = (parseFloat(currentInput) * -1).toString();
+    updateDisplay();
+    
+  }
+}
+
+function handlePercentClick() {
+  if (currentInput !== '' && previousInput !== '') {
+    const percentage = parseFloat(currentInput) * (parseFloat(previousInput) / 100);
+    currentInput = (parseFloat(previousInput) - percentage).toString();
+    updateDisplay();
+  }
+}
+
+function handleDeleteClick() {
+  currentInput = currentInput.slice(0, -1);
+  updateDisplay();
+}
+
+function performCalculation() {
+  const num1 = parseFloat(previousInput);
+  const num2 = parseFloat(currentInput);
+  switch (operator) {
+    case '+':
+      currentInput = (num1 + num2).toString();
+      break;
+    case '-':
+      currentInput = (num1 - num2).toString();
+      break;
+    case 'X':
+      currentInput = (num1 * num2).toString();
+      break;
+    case '/':
+      currentInput = (num1 / num2).toString();
+      break;
+  }
+  previousInput = '';
+  updateDisplay();
+}
 
 function buttonAnimation(event) {
-    const circle = document.createElement('div');
-    const x = event.layerX;
-    const y = event.layerY;
-    circle.classList.add('circle');
-    circle.style.left = `${x}px`;
-    circle.style.top = `${y}px`;
+  const circle = document.createElement('div');
+  const x = event.layerX;
+  const y = event.layerY;
+  circle.classList.add('circle');
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
 
-    this.appendChild(circle);
+  this.appendChild(circle);
 
-    circle.addEventListener('animationend', () => {
+  circle.addEventListener('animationend', () => {
     this.removeChild(circle);
-})
+  });
 }
 
 for (let button of buttons) {
-    button.addEventListener('click', buttonAnimation);
-};
+  button.addEventListener('click', function (event) {
+    buttonAnimation.call(this, event);
+    const buttonText = this.textContent;
+    switch (buttonText) {
+      case 'C':
+        handleClearClick();
+        break;
+      case '+/-':
+        handlePlusMinusClick();
+        break;
+      case '%':
+        handlePercentClick();
+        break;
+      case '=':
+        handleEqualsClick();
+        break;
+      case 'del':
+        handleDeleteClick();
+        break;
+      default:
+        if (!isNaN(parseFloat(buttonText)) || buttonText === '.') {
+          handleNumberClick(buttonText);
+        } else {
+          handleOperatorClick(buttonText);
+        }
+        break;
+    }
+  });
+}
+
